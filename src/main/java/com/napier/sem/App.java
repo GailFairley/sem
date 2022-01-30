c58b3592511fad5178aeb18861da3c5778b82ec0
Gail Fairley <40511352@live.napier.ac.uk>
2022-01-30 15:00:17 +0000

    Hi

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,5 +1,7 @@
+package com.napier.sem;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World 2");
+        System.out.println("Boo yah!");
     }
 }
+
