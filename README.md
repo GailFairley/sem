ed703eb08090b37f802949decc425b8363d31851
Gail Fairley <40511352@live.napier.ac.uk>
2022-02-08 17:07:34 +0000

    Connects to MDB ON SE-METHODS NETWORK

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 [![LICENSE](https://img.shields.io/github/license/GailFairley/sem.svg?style=flat-square)](https://github.com/GailFairley/sem/blob/master/LICENSE)
 [![Releases](https://img.shields.io/github/release/GailFairley/sem/all.svg?style=flat-square)](https://github.com/GailFairley/sem/releases)
 # Software Engineering Methods
-![GitHub Workflow Status (branch)](https://img.shields.io/github/workflow/status/GailFairley/sem/build/develop?style=flat-square)
+![GitHub Workflow Status (branch)](https://img.shields.io/github/workflow/status/GailFairley/sem/A%20workflow%20for%20my%20Hello%20World%20App/develop)
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,13 +4,13 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
+import java.util.concurrent.TimeUnit;
 
 public class App
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
@@ -26,6 +26,7 @@ public class App
         // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
+        TimeUnit.SECONDS.sleep(10);
     }
 }
 
